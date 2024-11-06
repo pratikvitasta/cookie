09a3b64dec05c88d268f7ba2a922cfe779d5a1b5
pratikvitasta <146178275+pratikvitasta@users.noreply.github.com>
2024-11-06 22:09:39 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,36 +14,20 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       // English
-      // "No",
-      // "Are you sure??🙄",
-      // "Really sure?🤔",
-      // "Are you positive???😶",
-      // "Please my love...🙏",
-      // "Just think about it🥺",
-      // "If you say no, I'll be very sad🙁",
-      // "I'll be very very sad😕",
-      // "I'll be very very very sad😓",
-      // "I'll be very very very very sad😖",
-      // "Ok fine😡, I'll stop asking...",
-      // "Just kidding, PLEASE SAY YES🙏",
-      // "I'll be very very very very very sad🥴",
-      // "You're breaking my heart😥😥😥",
-      
-      // Vietnamese
-      "Không",
-      "Em có chắc không??🙄",
-      "Em có thật sự chắc chắn?🤔",
-      "Em vẫn khẳng định???😶",
-      "Làm ơn đi! Tình yêu của tôi?🙏",
-      "Hãy suy nghĩ lại đi!🥺",
-      "Nếu em nói không, anh sẽ rất buồn🙁",
-      "Anh sẽ rất rất buồn😕",
-      "Anh sẽ rất rất rất buồn😓",
-      "Anh sẽ rất rất rất rất rất rất buồn😖",
-      "Được rồi😡, Anh sẽ không hỏi nữa...",
-      "Đùa thôi, VUI LÒNG NÓI CÓ ĐI🙏",
-      "Anh sẽ rất rất rất rất rất rất rất buồn🥴",
-      "Em đang làm tan nát trái tim tôi đó😥😥😥",
+      "No",
+      "Are you sure??🙄",
+      "Really sure?🤔",
+      "Are you positive???😶",
+      "Please my love...🙏",
+      "Just think about it🥺",
+      "If you say no, I'll be very sad🙁",
+      "I'll be very very sad😕",
+      "I'll be very very very sad😓",
+      "I'll be very very very very sad😖",
+      "Ok fine😡, I'll stop asking...",
+      "Just kidding, PLEASE SAY YES🙏",
+      "I'll be very very very very very sad🥴",
+      "You're breaking my heart😥😥😥",
     ];
 
     return phrases[noCount % phrases.length];
@@ -56,7 +40,7 @@ export default function Page() {
           <img src="https://gifdb.com/images/high/animated-bear-kiss-enngnq0gm2r405bt.webp" />
           <div className="text-[2rem] font-bold text-center leading-tight md:text-5xl md:leading-snug bg-gradient-to-r from-green-600 via-yellow-600 to-red-600 inline-block text-transparent bg-clip-text">
             {/* Thanks! Ok yay */}
-            Cảm ơn em! Tình yêu của anh đây rồi!!! hê hê
+            Wohoooo!!! I love you COOKIEEEE <3
           </div>
         </>
       ) : (
@@ -64,7 +48,7 @@ export default function Page() {
           <img className="h-[200px]" src="https://tenor.com/bIFCn.gif" />
           <h1 className="text-[2rem] text-center leading-tight md:text-5xl md:leading-snug bg-gradient-to-r from-green-600 via-yellow-600 to-red-600 inline-block text-transparent bg-clip-text">
             {/* Hey cookie, will you go on a date with me? */}
-            Em sẽ là tình yêu của anh chứ?
+            Hey cookie, will you go on a date with me?
           </h1>
           <div className="text-center space-y-5">
             <button
@@ -80,8 +64,8 @@ export default function Page() {
               className=" bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
             >
               {noCount === 0 ? 
-                // "No" 
-                "Không 😥"
+                "No" 
+                // "Không 😥"
                 : getNoButtonText()
               }
             </button>
